312f80c1325d50aaa9d93549ba82a2e8569eb8b8
Victor <webmaster@regalonatural.com>
2019-07-23 12:29:04 +0200

    Añadiendo DashBoard

--- loginPHP/controller/redirec.php
+++ loginPHP/controller/redirec.php
@@ -3,9 +3,11 @@
   session_start();
 
   if($_SESSION['cargo'] == 1){
-    header('location: ../view/admin/index.php');
+//    header('location: ../view/admin/index.php');
+      header('location: ../view/dashboard/index.html');
   }else if($_SESSION['cargo'] == 2){
-    header('location: ../view/user/index.php');
+//    header('location: ../view/user/index.php');
+    header('location: ../view/dashboard/index.html');
   }
 
  ?>
