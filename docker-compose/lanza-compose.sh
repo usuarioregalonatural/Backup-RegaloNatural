1fa837e85856be2208951d75988c2e607bce0899
Victor <webmaster@regalonatural.com>
2019-08-04 20:10:45 +0200

    incluido el yml del docker compose

---
+++ docker-compose/lanza-compose.sh
@@ -0,0 +1,10 @@
+docker stop propio_phpmyadmin_1
+docker rm propio_phpmyadmin_1
+docker stop propio_www_1
+docker rm propio_www_1
+docker stop propio_db_1
+docker rm propio_db_1
+
+docker-compose up -d
+
+
