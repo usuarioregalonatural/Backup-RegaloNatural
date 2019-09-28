dc35c419796136315f59d0b39a4f2aa48c04bd18
Victor <webmaster@regalonatural.com>
2019-09-28 12:16:38 +0200

    Adaptada nueva pantalla carritos

--- loginPHP/model/conexion.php
+++ loginPHP/model/conexion.php
@@ -9,8 +9,8 @@
         private $mysqli = '';
         private $usuario = 'root';
         private $clave = 'vmsn2004';
-//       private $server = 'backup.regalonatural.com:33060'; // Desarrollo
-        private $server ='backup_bbdd:3306'; // Produccion
+       private $server = 'backup.regalonatural.com:33060'; // Desarrollo
+//        private $server ='backup_bbdd:3306'; // Produccion
 
         private $db = 'login';
         public $num_registros;
