e617bb400ac222ea057951b74956005c87dc5370
Victor <webmaster@regalonatural.com>
2019-09-29 11:33:03 +0200

    Agregado para Google

--- config/config.php
+++ config/config.php
@@ -7,8 +7,8 @@
  */
 $ruta="miruta";
 
-//$entorno="Desarrollo";
-$entorno="Produccion";
+$entorno="Desarrollo";
+//$entorno="Produccion";
 // $ruta_sql="../SQL/";
  $ruta_json="../jsontrans/";
 # $gbl_servidor = 'backup.regalonatural.com:33060'; // Desarrollo
@@ -16,7 +16,7 @@ $entorno="Produccion";
 //$gbl_servidor='172.29.0.2:3306'; // Produccion
 
 if ($entorno=="Desarrollo"){
-    $gbl_servidor_backup='backup.regalonatural.com:33060'; // Produccion
+    $gbl_servidor_backup='backup.regalonatural.com:33060';
 } else {
     $gbl_servidor_backup = 'backup_bbdd:3306'; // Produccion
 }
--- index.php
+++ index.php
@@ -8,25 +8,24 @@
 //require_once ('./model/datosespacio.php');
 
 // isset verifica si existe una variable o eso creo xd
-if(isset($_SESSION['id'])){
-    header('location: ./loginPHP/controller/redirec.php');
-}
 
-// Recupero variables de la url
+// Anulado temporalmente
+//if(isset($_SESSION['id'])){
+//    header('location: ./loginPHP/controller/redirec.php');
+//}
+// fin Anulado temporalmente
 
-//$param1=$_GET["parametro1"];
-echo "Pagina Principal";
 
-//$datos= new Datosespacio();
-//$datos->Ver();
-//echo "num-registros: " . $datos->num_registros;
 
-//var_dump($datos->resultado);
-//echo "P: " .$datos->ocup_size;
-//echo "F: " .$datos->ocup_usado;
 
-//echo $objeto['ocup_size'];
+require_once __DIR__ . '/vendor/autoload.php';
 
+// 3- Si no hemos solicitado acceso en ningún momento, procedemos a solicitar acceso para recoger el access_token y el refresh_token para almacenarlos.
+$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+
+
+//echo $redirect_uri;
 
 
 ?>
